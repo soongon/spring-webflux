b53bf5d59547a18db773a71809d2ab0eda94bdbb
soongon <soongon@hucloud.co.kr>
2021-07-07 16:13:29 +0900

    initial commit

--- src/main/java/kr/re/kitri/springwebflux/controller/PostController.java
+++ src/main/java/kr/re/kitri/springwebflux/controller/PostController.java
@@ -2,8 +2,10 @@ package kr.re.kitri.springwebflux.controller;
 
 import kr.re.kitri.springwebflux.model.Post;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.List;
 public class PostController {
 
     @GetMapping("/posts")
-    public Flux<String> getPosts() {
+    public Flux<Post> getPosts() {
         List<Post> posts = Arrays.asList(
                 new Post(1, "안녕하세요", "good day"),
                 new Post(2, "안녕하세요", "good day"),
@@ -22,7 +24,13 @@ public class PostController {
         );
         return Flux
                 .fromIterable(posts)
-                .filter(t -> t.getPostId() > 3)
-                .map(t -> t.getTitle());
+                .filter(t -> t.getPostId() > 3);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Mono<Post> getPostById(@PathVariable int postId) {
+        Post post = new Post(postId, "안녕하세요", "반가워요..");
+        return Mono
+                .just(post);
     }
 }
